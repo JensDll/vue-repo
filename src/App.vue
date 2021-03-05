58d57dc53dcecb5ce2885c61d33f7a8beb4b39dd
Jens <jens@doellmann.com>
2021-03-05 16:22:15 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Title</h1>
+  <WithDot />
+  <WithoutDot />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import WithDot from "./.foo/Test.vue";
+import WithoutDot from "./foo/Test.vue";
+
+export default defineComponent({
+  components: {
+    WithDot,
+    WithoutDot,
+  },
+});
+</script>
+
+<style></style>
---
+++ src/foo/Test.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Without dot folder</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+const f = (x: number) => x;
+f("");
+
+export default defineComponent({});
+</script>
+
+<style></style>
